c205c952949dea1efb2657be2d51d4b1a8aefabf
aarondalao <aaron.dalao@gmail.com>
2023-03-12 16:54:19 +0800

    login page done and home page partially done. setting up sidebar and pins components

---
+++ node-2-node_frontend/src/components/index.js
@@ -0,0 +1,9 @@
+/*
+    Notes:
+    
+    use this file for importing all necessary components that will be used for the containers
+*/
+
+export { default as Sidebar } from './Sidebar';
+export { default as UserProfile } from './UserProfile';
+export { default as Login } from './Login';
